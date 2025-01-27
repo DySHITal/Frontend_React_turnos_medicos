55ba1346bc1ec855adaabc445252c0d56020ae91
Ana Fernandez <anny.fdz@gmail.com>
2025-01-27 01:37:35 -0300

    navbar redirecciona segun quien se loguea

--- alfa-project/src/views/components/Navbar.jsx
+++ alfa-project/src/views/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { useAuth } from '../../contexts/AuthContext';
 function Navbar() {
   const { isAuthenticated } = useAuth("state");
   const { logout } = useAuth("actions");
+  const { userType } = useAuth("state");
 
   return (
     <>
       
-      {/* Navbar */}
       <header className="bg-cyan-500 text-black py-4">
         <nav className="container mx-auto flex justify-between items-center px-4">
           <h1 className="text-2xl font-bold">Centro Médico Integral</h1>
@@ -18,7 +18,12 @@ function Navbar() {
               <a href="/inicio" className="hover:underline">Inicio</a>
             </li>
             <li>
-              <a href="/" className="hover:underline">Servicios</a>
+            <a
+                href={userType === "profesional" ? "/dashboard-profesional" : "/"}
+                className="hover:underline"
+              >
+                Servicios
+              </a>
             </li>
             <li>
               <a href="#contact" className="hover:underline">Contacto</a>
@@ -61,8 +66,6 @@ function Navbar() {
         </nav>
       </header>
 
-     
-    
     </>
   )
 }
